043d9212fc503a6f8c2abaa48aef98e9bb0aaa2e
Daniel Callow <33704076+CallowBrainProject@users.noreply.github.com>
2020-04-08 23:07:20 -0400

    Update Copy_to_bswift.sh

--- Copy_to_bswift.sh
+++ Copy_to_bswift.sh
@@ -1,1 +1,4 @@
-scp -rp BIDS dcallow@login.bswift.umd.edu:/data/bswift-1/dcallow
+# scp - secure copy [option] File_to_transfer Server_to_transfer_to:/Location/on/server
+
+scp -rp BIDS dcallow@login.***:/data/bswift-1/dcallow
+
